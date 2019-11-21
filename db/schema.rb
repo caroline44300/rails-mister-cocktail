d3b172496cbc919191bf3d132adff01ef9474b14
Caroline Douet <caro.do@wanadoo.fr>
2019-11-21 15:29:11 +0100

    updated models

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_21_134112) do
+ActiveRecord::Schema.define(version: 2019_11_21_142455) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,7 +22,7 @@ ActiveRecord::Schema.define(version: 2019_11_21_134112) do
   end
 
   create_table "doses", force: :cascade do |t|
-    t.string "desciption"
+    t.string "description"
     t.bigint "cocktail_id"
     t.bigint "ingredient_id"
     t.datetime "created_at", null: false
